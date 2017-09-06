ada147a224a76acc89e75f4087370cb14956befa
lavor-zl <1746898017@qq.com>
2017-09-06 17:11:10 +0800

    first commit

---
+++ src/main/java/com/lavor/jenkins/test1/PrintFunc.java
@@ -0,0 +1,42 @@
+package com.lavor.jenkins.test1;
+
+/**
+ * Created by lei.zeng on 2017/9/6.
+ */
+public class PrintFunc {
+    public int print1() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("print1" + i);
+        }
+        return 1;
+    }
+
+    public int print2() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("print2" + i);
+        }
+        return 2;
+    }
+
+    public int print3() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("print3" + i);
+        }
+        return 3;
+    }
+
+    public int print4() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("print4" + i);
+        }
+        return 4;
+    }
+
+    public int print5() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("print5" + i);
+        }
+        return 5;
+    }
+}
+
---
+++ src/test/java/com/lavor/jenkins/test1/PrintFuncTest.java
@@ -0,0 +1,31 @@
+package com.lavor.jenkins.test1;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by lei.zeng on 2017/9/6.
+ */
+public class PrintFuncTest {
+    PrintFunc printFunc=new PrintFunc();
+    @Test
+    public void print1() {
+        Assert.assertEquals(1,printFunc.print1());
+    }
+    @Test
+    public void print2() {
+        Assert.assertEquals(2,printFunc.print1());
+    }
+    @Test
+    public void print3() {
+        Assert.assertEquals(1,printFunc.print1());
+    }
+    @Test
+    public void print4() {
+        Assert.assertEquals(1,printFunc.print1());
+    }
+    @Test
+    public void print5() {
+        Assert.assertEquals(1,printFunc.print1());
+    }
+}
